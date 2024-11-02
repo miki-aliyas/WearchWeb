69fe292a6dbe5043726c887fa174a9a079e0ed5f
LAPTOP-M\81808 <miki.snoopy.729@gmail.com>
2024-11-02 14:49:36 +0900

    「Hello」と表示させる処理の実装

--- src/main/java/com/example/WearchWeb/WearchWebApplication.java
+++ src/main/java/com/example/WearchWeb/WearchWebApplication.java
@@ -2,9 +2,17 @@ package com.example.WearchWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class WearchWebApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WearchWebApplication.class, args);
