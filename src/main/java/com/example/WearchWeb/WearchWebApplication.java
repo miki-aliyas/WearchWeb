877aaf737875f64e2d09f076811a103264338a45
LAPTOP-M\81808 <miki.snoopy.729@gmail.com>
2024-11-02 13:08:41 +0900

    FirstCommit

---
+++ src/main/java/com/example/WearchWeb/WearchWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.WearchWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearchWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearchWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WearchWeb/WearchWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WearchWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearchWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
