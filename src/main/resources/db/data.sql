6bc5146c717c5c078147caba7550447615318380
LAPTOP-M\81808 <miki.snoopy.729@gmail.com>
2025-03-01 17:31:25 +0900

    カテゴリーAPI結果でカルーセル表示をできるように修正

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO Category (id, category_name, image_name) VALUES (1, 'Tops', 'frame_1');
-INSERT INTO Category (id, category_name, image_name) VALUES (2, 'Bottoms', 'frame_2');
-INSERT INTO Category (id, category_name, image_name) VALUES (3, 'Outer', 'frame_3');
-INSERT INTO Category (id, category_name, image_name) VALUES (4, 'Goods', 'frame_4');
-INSERT INTO Category (id, category_name, image_name) VALUES (5, 'Bag', 'frame_5');
+INSERT INTO Category (id, category_name, image_name) VALUES (1, 'Tops', 'frame_1.jpg');
+INSERT INTO Category (id, category_name, image_name) VALUES (2, 'Bottoms', 'frame_2.jpg');
+INSERT INTO Category (id, category_name, image_name) VALUES (3, 'Outer', 'frame_3.jpg');
+INSERT INTO Category (id, category_name, image_name) VALUES (4, 'Goods', 'frame_4.jpg');
+INSERT INTO Category (id, category_name, image_name) VALUES (5, 'Bag', 'frame_5.jpg');
 
 INSERT INTO Clothes_Item (id, category_id, image_name, size_name, dress_length, shoulder_width, sleeve_length, sleeve_width, neck_width, material) VALUES (1, 1, 'tshirt_1', 'FREE', 10, 10, 10, 10, 10, '(表地)：XX%\n(裏地)：XX%\n伸縮性：なし\n透け感：なし');
 INSERT INTO Clothes_Item (id, category_id, image_name, size_name, dress_length, shoulder_width, sleeve_length, sleeve_width, neck_width, material) VALUES (2, 1, 'tshirt_2', 'FREE', 10, 10, 10, 10, 10, '(表地)：XX%\n(裏地)：XX%\n伸縮性：なし\n透け感：なし');
